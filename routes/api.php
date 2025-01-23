d441839b9cd016ef0a46cfc29acbffc00574f5be
Umayantha93 <umayantha93@gmail.com>
2025-01-23 21:39:13 +0530

    create-controller-for-device-leasing-and-implement-get-method-of-each-device-activities

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\DeviceController;
+use App\Http\Controllers\LeasingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [DeviceController::class, 'register']);
+
+Route::get('/device/info/{id}', [LeasingController::class, 'getInfo']);
