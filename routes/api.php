d2ce70c74ca41907a83c73951d532c6a8fb4d07e
Umayantha93 <umayantha93@gmail.com>
2025-01-23 21:53:45 +0530

    feat:create-a-middleware-to-secure-get-method-and-update-method

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\LeasingController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [DeviceController::class, 'register']);
 
-Route::get('/device/info/{id}', [LeasingController::class, 'getInfo']);
-Route::put('/device/update/{id}', [LeasingController::class, 'updateLeasing']);
+Route::get('/device/info/{id}', [LeasingController::class, 'getInfo'])->middleware(Authenticate::class);
+Route::put('/device/update/{id}', [LeasingController::class, 'updateLeasing'])->middleware(Authenticate::class);
